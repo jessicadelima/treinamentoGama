9d3f2662bc7539f1a418e682f1a02a65cc55f868
jessicadelima <jessicadelima18@hotmail.com>
2020-08-14 11:08:47 -0300

    atualizacao projeto parte 2 lista cliente funcionando

--- aula14/back/src/main/java/br/gama/projagenda/controller/AgendamentoController.java
+++ aula14/back/src/main/java/br/gama/projagenda/controller/AgendamentoController.java
@@ -3,9 +3,12 @@ package br.gama.projagenda.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.gama.projagenda.dao.AgendamentoDAO;
@@ -25,15 +28,25 @@ public class AgendamentoController {
     }
 
     @GetMapping("/agendamentos/{id}")
-    public Agendamento getAgendamento(@PathVariable int id){
+    public Agendamento getAgendamentos(@PathVariable int id){
         Agendamento agendamento = dao.findById(id).orElse(null);
         return  agendamento;
     } 
 
     @GetMapping("/agendamentos/cliente/{nomeCliente}")
-    public Agendamento getAgendamento(@PathVariable String nomeCliente){
-        Agendamento agendamento = dao.findByNome(nomeCliente);
-        return  agendamento;
-    } 
+    public List<Agendamento> getAgendamentos(@PathVariable String nomeCliente){
+        List<Agendamento> agendamento = (List<Agendamento>)dao.findAllByNome(nomeCliente);
+        return agendamento;
+    }
+
+
+    @PostMapping("/novoagendamento")
+	public ResponseEntity<Agendamento> novoAgendamento(@RequestBody Agendamento Agendamento) {
+        Agendamento novo = dao.save(Agendamento);
+        
+        return ResponseEntity.status(200).build();
+	}
+
+        
 
 }
--- aula14/back/src/main/java/br/gama/projagenda/dao/AgendamentoDAO.java
+++ aula14/back/src/main/java/br/gama/projagenda/dao/AgendamentoDAO.java
@@ -1,9 +1,11 @@
 package br.gama.projagenda.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import br.gama.projagenda.model.Agendamento;
 
 public interface AgendamentoDAO extends CrudRepository<Agendamento, Integer> {
-    public Agendamento findByNome(String nomeCliente);
+    public List<Agendamento> findAllByNome(String nomeCliente);
 }
